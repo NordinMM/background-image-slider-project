ba27a5d6eaee51afebc2b92dfad798a185d2aebe
NORDIN M'HAMED <sewasy678@gmail.com>
2023-05-03 14:02:45 +0200

    acabdo

--- js/app.js
+++ js/app.js
@@ -6,4 +6,29 @@ const pictures = [
   "contBcg-4"
 ];
 
+let indexPhoto = 1;
+
 // buena suerte
+
+
+document.querySelector(".btn-left").addEventListener("click", () => {
+
+  indexPhoto--;
+  if (indexPhoto < 0) {
+    indexPhoto = pictures.length - 1;
+  }
+  document.querySelector(".img-container").style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
+  
+});
+
+document.querySelector(".btn-right").addEventListener("click", () => {
+  indexPhoto++;
+  if (indexPhoto >= pictures.length) {
+    indexPhoto = 0;
+  }
+
+  document.querySelector(".img-container").style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
+});
+
+
+document.querySelector(".img-container").style.backgroundImage = `url(../img/${pictures[indexPhoto]}.jpeg)`;
